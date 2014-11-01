331b780a821f89407f041492a88c7123bb2c52c1
Matt Richmond <mrichmond@wearetmbr.com>
2014-11-01 03:46:21 -0600

    few pushes

--- wp-content/themes/yfcw/front-page.php
+++ wp-content/themes/yfcw/front-page.php
@@ -10,13 +10,14 @@
 
 <div class="row">
 	<div class="col-xs-12 col-sm-offset-3 col-sm-6 text-center">
-		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+		<form class="paypal-donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 			<input type="hidden" name="cmd" value="_s-xclick">
 			<input type="hidden" name="hosted_button_id" value="MX2FUDMU7D3A6">
 			<input type="image" class="hidden" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 			<button class="btn btn-lg btn-success lrg-donate text-uppercase" type="submit">Donate Now</button>
 		</form>
+		<span class="label label-primary"><a href="#how-much-should-i-donate" title="How Much Should I Donate">How Much Should I Donate?</a></span>
 	</div>
 </div><!-- /row -->
 
@@ -50,5 +51,18 @@
 		</div>
 	</div>
 </div><!-- /row -->
+
+<div class="row" id="how-much-should-i-donate">
+	<div class="col-sm-10 col-sm-offset-1">
+		<h3>How much should I donate?</h3>
+		<p>Any donation is accepted, with our sincere thanks.  Please donate what you feel able to.  Any amount is tremendously helpful.  Here are some examples of how your donation can help:</p>
+		<ul class="list-unstyledx">
+			<li><strong>$5</strong> provides the seed money that is set aside for one month for one sister.  At the end of her year-long program, each $5 monthly savings adds up to a $60 seed-money investment in her new business.</li>
+			<li><strong>$30</strong> provides one month of a life-changing and life-saving program for a new sister.</li>
+			<li><strong>$90</strong> provides 3 months of a program through Women for Women for a new sister.  During this time, she will begin to receive help for the trauma she has endured, literacy skills, rights-awareness, and skill-specific training.</li>
+			<li><strong>$360</strong> will provide an entire year in the WfW program for one sister.  This program can literally mean the difference between life and death for her and for her children.  This will change everything for her!</li>
+		</ul>
+	</div>
+</div>
 
 <?php get_footer(); ?>
